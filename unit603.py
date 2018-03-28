a191e0db4ceab4c7b8719a7b31673034370f4b3e
Amin Zeina <amin.zeina@ocsbstudent.ca>
2018-03-28 14:03:35 -0400

    Initial commit

---
+++ unit603.py
@@ -0,0 +1,13 @@
+# Created by Amin Zeina
+# Created on Mar 2018
+
+age = int(input("Enter your age: "))
+
+if age > 17:
+	print('You can watch an R rated movie along')
+elif age >13:
+	print('You can watch a PG rated movie along')
+else:
+	print('You can watch a G rated movie along')
+
+input('Program end.')
