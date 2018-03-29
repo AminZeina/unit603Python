80a8e8dbbc1e880e78a528c87da1ecdb8d4a25c2
TheBellemTower <36240637+TheBellemTower@users.noreply.github.com>
2018-03-29 12:56:54 -0400

    Update unit603.py

--- unit603.py
+++ unit603.py
@@ -7,7 +7,9 @@ if age > 17:
 	print('You can watch an R rated movie along')
 elif age >13:
 	print('You can watch a PG rated movie along')
-else:
+elif age >5:
 	print('You can watch a G rated movie along')
+else:
+	print('dont wacth a movie read a book you are too young)
 
 input('Program end.')
